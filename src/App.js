4ff0e858ed53beee30398f22a4b6f2806699cabb
Dale <dale@copperviewci.com>
2025-04-12 01:16:19 -0600

    Fix Original File Name

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import useImage from "use-image";
 export default function Annotator() {
   const urlParams = new URLSearchParams(window.location.search);
   const imageUrl = urlParams.get("image") || "";
-  //const originalPath = urlParams.get("originalFileName") || "";
+  
   const rowId = urlParams.get("row") || "";
   const tableName = urlParams.get("table") || "";
   const jobId = urlParams.get("job") || "";
-  //const originalFileName = originalPath.split("/").pop();
+  
   const originalFileName = urlParams.get("originalFileName") || "";
 
 
